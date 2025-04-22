86e908c8a25ac8cf49bbe22342f33c1ff7114545
lthurler <leothurler@gmail.com>
2025-04-22 17:01:44 -0300

    hero animation

--- js/main.js
+++ js/main.js
@@ -102,10 +102,11 @@ const sr = ScrollReveal ({
     //reset:true /* Animations repeat */
 })
 
+sr.reveal(`#main-box`, { opacity: 0 })
 sr.reveal(`.contact__content`)
-sr.reveal(`#card-sobre-container,#social-container, #copyright, #cabo-card, #mesh, #externo, #ap`, {delay: 600, origin:'bottom', interval: 100})
+sr.reveal(`#card-sobre-container,#social-container, #copyright, #cabo-card, #mesh, #externo, #ap`, {delay: 600, origin:'bottom', interval: 500})
 sr.reveal(`#main-text, #footer-logo img, #footer-content`, {origin:'left'})
-sr.reveal(`#main-img, #footer-menu, #footer-map`, {origin:'right'})
+sr.reveal(`#main-img, #footer-menu, #footer-map, #main-link`, {origin:'right'})
 sr.reveal(`.qualification__content, .services__card`, { interval: 100 })
 
 // , #mesh-txt, #externo-img, #ap-txt , #mesh-img, #externo-txt, #ap-img
